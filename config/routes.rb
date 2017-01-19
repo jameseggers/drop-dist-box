5dc84d0fa4f83981cefa7e32d909358871cbc3bd
James Eggers <j@jameseggers.com>
2017-01-19 23:09:58 +0000

    added basic nice UI with materialize

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :node_types
   resources :nodes
   devise_for :users
-  resources :users
   resources :dist_files
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
