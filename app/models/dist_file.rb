59ee48f5004e70d7fd0313305765029646ce0be3
James Eggers <j@jameseggers.com>
2017-01-19 22:39:50 +0000

    works

--- app/models/dist_file.rb
+++ app/models/dist_file.rb
@@ -1,5 +1,5 @@
 class DistFile < ApplicationRecord
   has_attached_file :attached
-  validates_attachment :attached, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "text/plain"] }
+  validates_attachment :attached, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif", "text/plain", "application/pdf"] }
   belongs_to :user
 end
